c22b955a743040361d05748df66c4b2521180e1e
Oleg Lipin <oleg.lipin@kohls.com>
2017-12-04 15:32:08 +0300

    Artifact resolver have been added

---
+++ scripts/artidown.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+repo="wget http://10.0.2.10:8081"
+groupId=$1
+artifactId=$2
+version=$3
+
+# optional
+classifier=$4
+type=$5
+
+if [[ $type == "" ]]; then
+  type="jar"
+fi
+if [[ $classifier != "" ]]; then
+  classifier="-${classifier}"
+fi
+
+groupIdUrl="${groupId//.//}"
+filename="${artifactId}-${version}${classifier}.${type}"
+
+if [[ ${version} == *"SNAPSHOT"* ]]; then repo_type="snapshots"; else repo_type="releases"; fi
+
+if [[ $repo_type == "releases" ]]
+ then
+   wget --no-check-certificate "${repo}/repository/releases/${groupIdUrl}/${artifactId}/${version}/${artifactId}-${version}${classifier}.${type}" -O ${filename} -k
+ else
+   versionTimestamped=$(wget -q -O- --no-check-certificate "${repo}/repository/snapshots/${groupIdUrl}/${artifactId}/${version}/maven-metadata.xml" | grep -m 1 \<value\> | sed -e 's/<value>\(.*\)<\/value>/\1/' | sed -e 's/ //g')
+
+   wget --no-check-certificate "${repo}/repository/snapshots/${groupIdUrl}/${artifactId}/${version}/${artifactId}-${versionTimestamped}${classifier}.${type}" -O ${filename}
+ fi
