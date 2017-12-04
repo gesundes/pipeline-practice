03cacdb27b96d012a398795633b58611354c0766
Oleg Lipin <oleg.lipin@kohls.com>
2017-12-04 15:59:56 +0300

    Artifact resolver have been changed to use Nexus 3

--- scripts/artidown.sh
+++ scripts/artidown.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-repo="wget http://10.0.2.10:8081"
+repo="http://10.0.2.10:8081"
 groupId=$1
 artifactId=$2
 version=$3
@@ -23,9 +23,9 @@ if [[ ${version} == *"SNAPSHOT"* ]]; then repo_type="snapshots"; else repo_type=
 
 if [[ $repo_type == "releases" ]]
  then
-   wget --no-check-certificate "${repo}/repository/releases/${groupIdUrl}/${artifactId}/${version}/${artifactId}-${version}${classifier}.${type}" -O ${filename} -k
+   wget --no-check-certificate "${repo}/repository/maven-releases/${groupIdUrl}/${artifactId}/${version}/${artifactId}-${version}${classifier}.${type}" -O ${filename} -k
  else
-   versionTimestamped=$(wget -q -O- --no-check-certificate "${repo}/repository/snapshots/${groupIdUrl}/${artifactId}/${version}/maven-metadata.xml" | grep -m 1 \<value\> | sed -e 's/<value>\(.*\)<\/value>/\1/' | sed -e 's/ //g')
+   versionTimestamped=$(wget -q -O- --no-check-certificate "${repo}/repository/maven-snapshots/${groupIdUrl}/${artifactId}/${version}/maven-metadata.xml" | grep -m 1 \<value\> | sed -e 's/<value>\(.*\)<\/value>/\1/' | sed -e 's/ //g')
 
-   wget --no-check-certificate "${repo}/repository/snapshots/${groupIdUrl}/${artifactId}/${version}/${artifactId}-${versionTimestamped}${classifier}.${type}" -O ${filename}
+   wget --no-check-certificate "${repo}/repository/maven-snapshots/${groupIdUrl}/${artifactId}/${version}/${artifactId}-${versionTimestamped}${classifier}.${type}" -O ${filename}
  fi
